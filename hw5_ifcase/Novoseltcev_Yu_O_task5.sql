757c9377a38d37928ef84b61f657f1f477e75bb0
Yuri_Novoseltsev <veloren1@gmail.com>
2025-04-19 10:40:29 +0300

    Домашнее задание 5: Условия

--- hw5_ifcase/Novoseltcev_Yu_O_task5.sql
+++ hw5_ifcase/Novoseltcev_Yu_O_task5.sql
@@ -6,9 +6,9 @@
 --1. Создание платежа
 declare
   v_description     varchar2(100 char) := 'Платеж создан.';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := 1;
-  c_create_status   number(10)         := 0;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := 1;
+  c_create_status   number(10) := 0;
 begin
   dbms_output.put_line(v_description||' Статус: '||c_create_status
                                     ||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss:ff3'));
@@ -20,9 +20,9 @@ end;
 declare
   v_description     varchar2(100 char) := 'Сброс платежа в "ошибочный статус" с указанием причины.';
   v_reason          varchar2(200 char) := 'недостаточно средств';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := 1;
-  c_error_status    number(10)         := 2;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := 1;
+  c_error_status    number(10) := 2;
 begin  
   dbms_output.put_line (v_description||' Статус: '||c_error_status||'. Причина: '||v_reason
                                      ||'. Дата создания записи (День): '||to_char(v_current_time,'DAY'));
@@ -42,9 +42,9 @@ end;
 declare
   v_description     varchar2(100 char) := 'Отмена платежа с указанием причины.';
   v_reason          varchar2(200 char) := 'ошибка пользователя';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := 1;
-  c_cancel_status   number(10)         := 3;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := 1;
+  c_cancel_status   number(10) := 3;
 begin  
   dbms_output.put_line (v_description||' Статус: '||c_cancel_status||'. Причина: '||v_reason
                                      ||'. Дата создания записи (Месяц): '||to_char(v_current_time,'MONTH'));
@@ -62,9 +62,9 @@ end;
 --4. Платеж завершен успешно
 declare
   v_description     varchar2(100 char) := 'Успешное завершение платежа.';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := 1;
-  c_success_status  number(10)         := 1;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := 1;
+  c_success_status  number(10) := 1;
 begin  
   dbms_output.put_line (v_description||' Статус: '||c_success_status
                                      ||'. Дата создания записи (1-ый день месяца): '||trunc(v_current_time,'MONTH'));
@@ -79,8 +79,8 @@ end;
 --5. Добавление или обновление данных платежа по списку
 declare
   v_description     varchar2(100 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := 1;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := 1;
 begin  
   dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
   if v_payment_id is null then
@@ -94,8 +94,8 @@ end;
 --6. Удаление деталей платежа списку
 declare
   v_description     varchar2(100 char) := 'Детали платежа удалены по списку id_полей';
-  v_current_time    timestamp          := sysdate;
-  v_payment_id      number(38)         := null;
+  v_current_time    timestamp := sysdate;
+  v_payment_id      number(38) := null;
 begin  
   dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
   if v_payment_id is null then
