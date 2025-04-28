c6ecd45e23d8813dda0ce2946d4976fdb99e5b3b
Yuri_Novoseltsev <veloren1@gmail.com>
2025-04-28 22:10:59 +0300

    Домашнее задание 9: Коллекции (#11)
    * Домашнее задание 9: Коллекции
    
    * Домашнее задание 9: Коллекции

--- Novoseltcev_API_Payment.sql
+++ Novoseltcev_API_Payment.sql
@@ -8,8 +8,14 @@ declare
   v_description     varchar2(100 char) := 'Платеж создан.';
   v_current_time    timestamp := sysdate;
   v_payment_id      PAYMENT.PAYMENT_ID%type := 1;
+  
+  v_payment_detail_array t_payment_detail_array := t_payment_detail_array();
+  
   c_create_status   PAYMENT.STATUS%type := 0;
 begin
+  v_payment_detail_array.extend(2);
+  v_payment_detail_array(1) := t_payment_detail(1,'СБП');
+  v_payment_detail_array(2) := t_payment_detail(2,'91.231.88.28');
   dbms_output.put_line(v_description||' Статус: '||c_create_status
                                     ||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss:ff3'));
   dbms_output.put_line('ID платежа = '||v_payment_id);
@@ -24,17 +30,15 @@ declare
   v_payment_id      PAYMENT.PAYMENT_ID%type := 1;
   c_error_status    PAYMENT.STATUS%type := 2;
 begin  
-  dbms_output.put_line (v_description||' Статус: '||c_error_status||'. Причина: '||v_reason
-                                     ||'. Дата создания записи (День): '||to_char(v_current_time,'DAY'));
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
+  elsif v_reason is null then
+    dbms_output.put_line('Причина не может быть пустой');
   else
+    dbms_output.put_line (v_description||' Статус: '||c_error_status||'. Причина: '||v_reason
+                                       ||'. Дата создания записи (День): '||to_char(v_current_time,'DAY'));
     dbms_output.put_line('ID платежа = '||v_payment_id);
   end if;
-  if v_reason is null then
-    dbms_output.put_line('Причина не может быть пустой');
-  end if;
-  
 end;
 /
 
@@ -46,15 +50,14 @@ declare
   v_payment_id      PAYMENT.PAYMENT_ID%type := 1;
   c_cancel_status   PAYMENT.STATUS%type := 3;
 begin  
-  dbms_output.put_line (v_description||' Статус: '||c_cancel_status||'. Причина: '||v_reason
-                                     ||'. Дата создания записи (Месяц): '||to_char(v_current_time,'MONTH'));
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
-  else
-    dbms_output.put_line('ID платежа = '||v_payment_id);
-  end if;
-  if v_reason is null then
+  elsif v_reason is null then
     dbms_output.put_line('Причина не может быть пустой');
+  else
+    dbms_output.put_line (v_description||' Статус: '||c_cancel_status||'. Причина: '||v_reason
+                                       ||'. Дата создания записи (Месяц): '||to_char(v_current_time,'MONTH'));
+    dbms_output.put_line('ID платежа = '||v_payment_id);
   end if;
 end;
 /
@@ -66,11 +69,11 @@ declare
   v_payment_id      PAYMENT.PAYMENT_ID%type := 1;
   c_success_status  PAYMENT.STATUS%type := 1;
 begin  
-  dbms_output.put_line (v_description||' Статус: '||c_success_status
-                                     ||'. Дата создания записи (1-ый день месяца): '||trunc(v_current_time,'MONTH'));
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
   else
+    dbms_output.put_line (v_description||' Статус: '||c_success_status
+                                       ||'. Дата создания записи (1-ый день месяца): '||trunc(v_current_time,'MONTH'));
     dbms_output.put_line('ID платежа = '||v_payment_id);
   end if;
 end;
@@ -81,11 +84,15 @@ declare
   v_description     varchar2(100 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
   v_current_time    timestamp := sysdate;
   v_payment_id      PAYMENT.PAYMENT_ID%type := 1;
+  
+  v_payment_detail_array t_payment_detail_array := t_payment_detail_array();
 begin  
-  dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
+  v_payment_detail_array.extend(1);
+  v_payment_detail_array(1) := t_payment_detail(3,'Долг');
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
   else
+    dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
     dbms_output.put_line('ID платежа = '||v_payment_id);
   end if;
 end;
@@ -96,11 +103,13 @@ declare
   v_description     varchar2(100 char) := 'Детали платежа удалены по списку id_полей';
   v_current_time    timestamp := sysdate;
   v_payment_id      PAYMENT.PAYMENT_ID%type := null;
+  
+  v_number_array    t_number_array := t_number_array(1,2,4);
 begin  
-  dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
   if v_payment_id is null then
     dbms_output.put_line('ID объекта не может быть пустым');
   else
+    dbms_output.put_line (v_description||'. Дата создания записи: '||to_char(v_current_time,'dd-mm-yyyy hh24:mi:ss'));
     dbms_output.put_line('ID платежа = '||v_payment_id);
   end if;
 end;
---
+++ Types/Novoseltcev_Yu_O_t_payment_detail_array.tps
@@ -0,0 +1,2 @@
+create or replace type t_payment_detail_array is table of t_payment_detail
+/
